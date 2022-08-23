e73b6f17c7a27ffd3cfa98c7580cf378408708cd
Neil Rifarial <111344245+Neillobino18@users.noreply.github.com>
2022-08-23 14:42:23 +0800

    Create App.java

---
+++ without_constructor/App.java
@@ -0,0 +1,48 @@
+public class App {
+    public static void main(String[] args) {
+
+        Character knight = new Character();
+
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "Chen";
+        knight.sayMyName();
+        knight.sayMyStrength();
+        knight.sayMyAgility();
+        knight.sayMyintelligence();
+
+        Character mage = new Character();
+
+        mage.strength = 0;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Akasha";
+        mage.sayMyName();
+        mage.sayMyStrength();
+        mage.sayMyAgility();
+        mage.sayMyintelligence();
+
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        thief.name = "Gondar";
+        thief.sayMyName();
+        thief.sayMyStrength();
+        thief.sayMyAgility();
+        thief.sayMyintelligence();
+
+        Character healer = new Character();
+        
+        healer.strength = 10;
+        healer.agility = 7;
+        healer. intelligence = 0;
+        healer.name = "Belerick";
+        healer.sayMyName();
+        healer.sayMyStrength();
+        healer.sayMyAgility();
+        healer.sayMyintelligence();
+    }
+}
